aa2d5b920ee3eae72440b3b0571a08dd7dda3a16
SHyoJun <42951642+SHyoJun@users.noreply.github.com>
2019-08-31 17:32:50 +0900

    7번 문제 해결

--- test.py
+++ test.py
@@ -15,8 +15,29 @@ def avrege(*b):
 
 print(avrege(1,2,3,4,5,6,7,9))
 # 3번문제
-input1 = input("첫번째 숫자를 입력하세요:")
-input2 = input("두번째 숫자를 입력하세요:")
+# input1 = input("첫번째 숫자를 입력하세요:")
+# input2 = input("두번째 숫자를 입력하세요:")
+#
+# total = int(input1) + int(input2)
+# print("두 수의 합은 %s 입니다" % total)
+# 4번문제
+# print("you", "need", "python")
+# #5번문제
+# f1 = open("test1.txt", 'w')
+# f1.write("Life is too short")
+# f1.close()
+# f2 = open("test1.txt", 'r')
+# print(f2.read())
+# f2.close()
+# 6번문제
+# with open("test2.txt", 'a') as f:
+#     f.write(input("입력바랍니다 :")+"\n")
+# 7번문제
+with open("test.txt", 'r') as f:
+    line = f.readlines()
+    for i in range(0,len(line)):
+        line[i] = line[i].replace("java", "python")
 
-total = int(input1) + int(input2)
-print("두 수의 합은 %s 입니다" % total)
+with open("test.txt", 'w') as f:
+    for i in line:
+        f.writelines(i)
