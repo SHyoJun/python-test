49a00c2298b37f915408b4dbb643ab220f45bcde
SHyoJun <42951642+SHyoJun@users.noreply.github.com>
2019-08-31 16:51:48 +0900

    3번 문제 해결

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# 1번문제
 def is_odd(a):
     if a%2==0:
         print('짝')
@@ -5,6 +6,7 @@ def is_odd(a):
         print('홀')
 
 is_odd(2)
+# 2번문제
 def avrege(*b):
     num = 0
     for i in b:
@@ -12,3 +14,9 @@ def avrege(*b):
     return num/len(b)
 
 print(avrege(1,2,3,4,5,6,7,9))
+# 3번문제
+input1 = input("첫번째 숫자를 입력하세요:")
+input2 = input("두번째 숫자를 입력하세요:")
+
+total = int(input1) + int(input2)
+print("두 수의 합은 %s 입니다" % total)
